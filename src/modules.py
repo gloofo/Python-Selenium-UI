1bf7526b70feea255439b3a8583266e75db1e07f
gloofo <gloofo@gloofo.glof>
2023-07-08 14:00:44 +0800

    Added loop and screenshot per page

--- src/modules.py
+++ src/modules.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
 from time import sleep
 
 @pytest.fixture
@@ -25,6 +26,9 @@ def goto(setup, data):
     getData = data['page']['main']
     setup.get(getData)
     setup.maximize_window()
+    
+    navigate(setup, data)
+    menuLists(setup, data)
     
 def navigate(setup, data):
     element = setup.find_element(By.CSS_SELECTOR, data['page']['search'])
@@ -47,8 +51,11 @@ def navigate(setup, data):
     
     wait(setup, data, "sidebar")
     scroll(setup, -1000)
+    sleep(2)
+    scroll(setup, 0)
     
 def wait(setup, data, locator):
+    
     return WebDriverWait(setup, 10).until(EC.visibility_of_element_located(
         (By.CLASS_NAME, data['page'][f'{locator}'])
     ))
@@ -58,5 +65,26 @@ def scroll(setup, scroll_amount: int):
     
 def menuLists(setup, data):
     elements = setup.find_elements(By.CSS_SELECTOR, data['page']['list'])
+    x = 0
     for element in elements:
         element.click()
+        scrolltoBottom(setup)
+        sleep(1)
+        x += 1
+        setup.execute_script(f"window.scrollTo(0, 0);")
+        setup.save_screenshot(f"screenshots/capture {x}.png")
+
+    setup.quit()
+        
+def scrolltoBottom(setup):
+    h = setup.execute_script("return document.body.scrollHeight")
+    while True:
+        setup.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        sleep(2)
+        nh = setup.execute_script("return document.body.scrollHeight")
+        if nh == h:
+            break
+        h = nh
+        
+    
+    
--- test_pytest.py
+++ test_pytest.py
@@ -2,5 +2,3 @@ from src.modules import *
 
 def test_page(setup, data):
     goto(setup, data)
-    navigate(setup, data)
-    menuLists(setup, data)
