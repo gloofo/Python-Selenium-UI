b6edffbc08c0f38b615553e6082e65a47279a7fa
gloofo <gloofo@gloofo.glof>
2023-07-15 13:39:28 +0800

    Changes

--- src/source.py
+++ src/source.py
@@ -2,41 +2,43 @@
 from src.imports import *
 
 #read and open yaml file for locators variable.
-@pytest.fixture
 def data():
     with open("resources/locators.yaml") as file:
-        r =  yaml.load(file, Loader=yaml.FullLoader)
-
-    return r
+        load =  yaml.load(file, Loader=yaml.FullLoader)
+    return load
 
 #Initialized webdriver
 @pytest.fixture(scope='function')
 def setup():
+    options = Options()
+    options.add_argument("--hide-scrollbars")
+    options.add_argument("--disable=infobars")
+    
     print("TEST STARTED")
-    yield webdriver.Chrome()
+    yield webdriver.Chrome(options=options)
     print("TEST ENDED")
 
 #main method for test case.
-def main(setup, data):
-    getData = data['page']['main']
+def main(setup):
+    getData = data()['page']['main']
     setup.get(getData)
     setup.maximize_window()
     
-    navigate(setup, data)
-    menuLists(setup, data)
+    navigate(setup)
+    menuLists(setup)
 
 #navigates through the page 
-def navigate(setup, data):
+def navigate(setup):
     #search
-    element = setup.find_element(By.CSS_SELECTOR, data['page']['search'])
+    element = setup.find_element(By.CSS_SELECTOR, data()['page']['search'])
     element.click()
-    wait(setup, data, "searchContainer")
-    search = setup.find_element(By.ID, data['page']['searchMain'])
+    wait(setup, "searchContainer")
+    search = setup.find_element(By.ID, data()['page']['searchMain'])
     search.send_keys("Python")
     sleep(1)
     
     #hover results until the end and go back to the first element then click.
-    hoverElements = setup.find_elements(By.CSS_SELECTOR, data['page']['results'])
+    hoverElements = setup.find_elements(By.CSS_SELECTOR, data()['page']['results'])
     for i in hoverElements:
         hover = ActionChains(setup).move_to_element(i)
         hover.perform()
@@ -47,7 +49,7 @@ def navigate(setup, data):
         hover.click().perform()
         assert setup.current_url == "https://playwright.dev/docs/languages#python"
     
-    wait(setup, data, "sidebar")
+    wait(setup, "sidebar")
     
     #call scroll method
     scroll(setup, -1000)
@@ -55,10 +57,10 @@ def navigate(setup, data):
     scroll(setup, 0)
 
 #waits an element (locator) 
-def wait(setup, data, locator):
+def wait(setup, locator):
     
     return WebDriverWait(setup, 10).until(EC.visibility_of_element_located(
-        (By.CLASS_NAME, data['page'][f'{locator}'])
+        (By.CLASS_NAME, data()['page'][f'{locator}'])
     ))
 
 #scroll through the page scroll_amount value
@@ -66,12 +68,13 @@ def scroll(setup, scroll_amount: int):
     return setup.execute_script(f"window.scrollBy(0, {scroll_amount});")
 
 #clicks every page and save a screenshot
-def menuLists(setup, data):
-    elements = setup.find_elements(By.CSS_SELECTOR, data['page']['list'])
+def menuLists(setup):
+    elements = setup.find_elements(By.CSS_SELECTOR, data()['page']['list'])
     x = 0
     for element in elements:
         element.click()
         scrolltoBottom(setup)
+            
         sleep(1)
         x += 1
         setup.execute_script(f"window.scrollTo(0, 0);")
--- test_.py
+++ test_.py
@@ -1,4 +1,4 @@
 from src.source import *
 
-def test_playwrightdev(setup, data):
-    main(setup, data)
+def test_playwrightdev(setup):
+    main(setup)
